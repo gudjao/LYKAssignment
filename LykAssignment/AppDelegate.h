dbddb013b3ffbbfdf54b920dec0ebba4ccf5c944
Juston Paul Alcantara <jpalcantara29@gmail.com>
2017-11-07 15:52:03 +0800

    Initial Commit

---
+++ LykAssignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LykAssignment
+//
+//  Created by Juston Paul Alcantara on 07/11/2017.
+//  Copyright © 2017 Juston Paul Alcantara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LykAssignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LykAssignment
+//
+//  Created by Juston Paul Alcantara on 07/11/2017.
+//  Copyright © 2017 Juston Paul Alcantara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
